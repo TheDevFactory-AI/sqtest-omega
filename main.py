f067af2e1e063d398782ef641c909c67a91656da
primus <primus@10.200.32.2>
2023-11-09 16:48:35 -0500

    Initialized main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+def add_numbers(a, b):
+    return a + b
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero."
+    return a / b
+def subtract_numbers(a, b):
+    return a - b
+
+
+
+result = add_numbers(5, 3)
+print("Addition result:", result)
+
+result = subtract_numbers(8, 4)
+print("Subtraction result:", result)
+
+result = multiply_numbers(2, 6)
+print("Multiplication result:", result)
+
+result = divide_numbers(10, 2)
+print("Division result:", result)
